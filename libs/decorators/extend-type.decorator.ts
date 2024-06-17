2e31108ae0601353a5a5dca333456cd98a1f923b
Nguyen Tien Dung <dung@hodfords.com>
2024-06-17 13:46:53 +0700

    feat: add extend type

--- libs/decorators/extend-type.decorator.ts
+++ libs/decorators/extend-type.decorator.ts
@@ -4,7 +4,8 @@ export function ExtendType() {
     return function ClassDecorator<T extends { new (...args: any[]): {} }>(constructor: T) {
         let properties = propertyStorage.get(Object.getPrototypeOf(constructor));
         propertyStorage.delete(Object.getPrototypeOf(constructor));
-        propertyStorage.set(constructor, properties);
+        let newProperties = propertyStorage.get(constructor) || [];
+        propertyStorage.set(constructor, [...properties, ...newProperties]);
 
         if (sdkDtos.has(Object.getPrototypeOf(constructor))) {
             sdkDtos.delete(Object.getPrototypeOf(constructor));
