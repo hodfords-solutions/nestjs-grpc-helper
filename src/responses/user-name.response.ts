e2af42e0f5301bf69d4c107ae2797d12087c1f46
Nguyen Tien Dung <dung@hodfords.com>
2024-06-17 14:13:06 +0700

    feat: add extend type

--- src/responses/user-name.response.ts
+++ src/responses/user-name.response.ts
@@ -2,6 +2,8 @@ import { UserResponse } from './user.response';
 import { ExtendType, MockMethod, PickResponseType, Property } from '@hodfords/nestjs-grpc-helper';
 import { IsOptional, IsString } from 'class-validator';
 import { IntersectionResponseType } from '../../libs/type-helpers/intersection-type.helper';
+import { OmitResponseType } from '../../libs/type-helpers/omit-type.helper';
+import { PartialResponseType } from '../../libs/type-helpers/partial-type.helper';
 
 @ExtendType()
 export class UserNameResponse extends PickResponseType(UserResponse, ['name', 'type']) {}
@@ -23,3 +25,14 @@ export class UserFullResponse extends IntersectionResponseType(UserNameResponse,
     @IsOptional()
     state?: string;
 }
+
+@ExtendType()
+export class UserAddressResponse extends OmitResponseType(UserFullResponse, ['name', 'type']) {
+    @Property({ type: String, required: true })
+    @MockMethod('faker.datatype.string', [10])
+    @IsString()
+    country: string;
+}
+
+@ExtendType()
+export class UserPartialResponse extends PartialResponseType(UserAddressResponse) {}
