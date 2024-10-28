9b2eabca9ef8acab7182c8ecb7600aa5ad580dff
hodfords-phuong-vo-be <phuong.vt@hodfords.com>
2024-10-28 22:52:27 +0700

    feat: support request initializer

--- lib/sdk-stub/types/microservice-option.type.ts
+++ lib/sdk-stub/types/microservice-option.type.ts
@@ -1,13 +1,15 @@
+import { Metadata } from '@grpc/grpc-js';
+
 export type MicroserviceModuleOptionType = {
     timeout: number;
     url: string;
     ssl?: boolean;
     maxReceiveMessageLength?: number;
     shouldLoadEmptyArray?: boolean;
-    enableLanguageMetaData?: boolean;
+    requestInitializer?: (metadata: Metadata) => void;
 };
 
 export type MicroserviceClientOptionType = {
     timeout: number;
-    enableLanguageMetaData?: boolean;
+    requestInitializer?: (metadata: Metadata) => void;
 };
