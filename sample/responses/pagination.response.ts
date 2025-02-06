33b0d6ee8e3972fb78d77049ac915c2e0712b370
jc-cong-nguyen <nguyen.cong@james-chase.com>
2025-02-06 15:32:18 +0700

    vendor: upgrade nestjs version to 11

--- sample/responses/pagination.response.ts
+++ sample/responses/pagination.response.ts
@@ -4,22 +4,22 @@ import { MockMethod, MockSample, Property } from '@hodfords/nestjs-grpc-helper';
 export abstract class PaginationResponse {
     abstract items: any[];
 
-    @Property({ type: 'int32' })
+    @Property({ type: String, format: 'int32' })
     @MockMethod('faker.number.int', [5, 10])
     @IsNumber()
     total: number;
 
-    @Property({ type: 'int32' })
+    @Property({ type: String, format: 'int32' })
     @MockMethod('faker.number.int', [5, 10])
     @IsNumber()
     lastPage: number;
 
-    @Property({ type: 'int32' })
+    @Property({ type: String, format: 'int32' })
     @MockMethod('faker.number.int', [5, 10])
     @IsNumber()
     perPage: number;
 
-    @Property({ type: 'int32' })
+    @Property({ type: String, format: 'int32' })
     @MockSample(1)
     @IsNumber()
     currentPage: number;
