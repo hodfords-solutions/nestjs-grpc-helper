1f118043d2d69bfed276204237e3af6f155ac466
jc-cong-nguyen <nguyen.cong@james-chase.com>
2025-02-06 15:32:18 +0700

    fix: conflict

--- lib/helpers/proto-type.helper.ts
+++ lib/helpers/proto-type.helper.ts
@@ -1,15 +1,15 @@
 import { isFunction, isString } from '@nestjs/common/utils/shared.utils';
-import { ApiPropertyOptions } from '@nestjs/swagger/dist/decorators/api-property.decorator';
 import { isEnumProperty } from './api-property.helper';
+import { PropertyOptionType } from '@hodfords/nestjs-grpc-helper';
 
-export function convertProtoTypeToSwagger(option: ApiPropertyOptions): ApiPropertyOptions['type'] {
+export function convertProtoTypeToSwagger(option: PropertyOptionType): PropertyOptionType['type'] | string {
     if (isFunction(option.type)) {
         return option.type;
     }
     return convertProtoTypeToTypescript(option, false);
 }
 
-export function convertProtoTypeToTypescript(option: ApiPropertyOptions, isGenerate?: boolean): string {
+export function convertProtoTypeToTypescript(option: PropertyOptionType, isGenerate?: boolean): string {
     let type = option.type;
     if (isFunction(type)) {
         if (type.name === 'type' && isGenerate) {
@@ -23,8 +23,8 @@ export function convertProtoTypeToTypescript(option: ApiPropertyOptions, isGener
     }
 
     if (isString(option.type)) {
-        const numberTypes = ['int32', 'fixed32', 'uint32', 'int64', 'fixed64', 'uint64', 'float', 'double', 'float'];
-        if (numberTypes.includes(option.type)) {
+        const numberTypes = ['int32', 'fixed32', 'uint32', 'int64', 'fixed64', 'uint64', 'float', 'double'];
+        if (numberTypes.includes(option.format)) {
             type = 'number';
         }
 
